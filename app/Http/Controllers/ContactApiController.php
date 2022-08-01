6939b6c7a832ba115d50c59d1bbc88c57c9b4c8e
Putra Prima Arhandi <putraprima@gmail.com>
2022-08-01 19:33:31 +0700

    Finished Api Contact Code

---
+++ app/Http/Controllers/ContactApiController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactRequest;
+use App\Http\Resources\ContactResource;
+use App\Models\Contact;
+
+class ContactApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return ContactResource::collection(Contact::paginate(15));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactRequest $request)
+    {
+        //
+        return new ContactResource(Contact::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact $contact)
+    {
+        //
+        return  new ContactResource($contact);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ContactRequest $request, Contact $contact)
+    {
+        //
+        $contact->update($request->validated());
+        return new ContactResource($contact);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return response()->noContent();
+    }
+}
